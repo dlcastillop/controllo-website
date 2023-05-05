332a256b16c66237e3fd86e851517961f29053fe
Daniel Castillo <dlcastillo3015@gmail.com>
2023-05-05 06:00:46 -0400

    build: google analytics integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown()],
 });
