a80553149f39c4fec7e3c2098c9b2e6b6368be65
Daniel Castillo <dlcastillo3015@gmail.com>
2023-05-07 08:17:24 -0400

    build: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()],
+  site: "https://controllo.dlcastillop.com",
+  integrations: [tailwind(), partytown(), sitemap()],
 });
